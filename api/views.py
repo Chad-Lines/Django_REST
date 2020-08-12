9db054d2de35f5bf8f305f6b1e1ecfebdd266fef
Chad <chad.lines1@gmail.com>
2020-08-12 15:13:59 -0600

    1.1 Basic API response with DRF

--- api/views.py
+++ api/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse("API BASE POINT", safe=False)
+    # These are the url patterns we're going to create and provide
+    api_urls = {
+        'List': '/task-.list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update':'/task-update/<str:pk>/',
+        'Delete':'/task-delete/<str:pk>/'
+    }
+
+    return Response(api_urls)
