6788064e277c734e4c63f3f0de09c3c06dba76d0
Chad <chad.lines1@gmail.com>
2020-08-13 14:18:58 -0600

    2.1 Basic GET for Task

--- api/views.py
+++ api/views.py
@@ -5,6 +5,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 
+from .models import Task
+
 @api_view(['GET'])
 def apiOverview(request):
     # These are the url patterns we're going to create and provide
@@ -17,3 +19,9 @@ def apiOverview(request):
     }
 
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()                      # These are the tasks that exist
+    serializer = TaskSerializer(tasks, many=True)   # This is the serializer defined in serializers.py
+    return Response(serializer.data)                # This returns the data serialized on the above line
